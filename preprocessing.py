e6f38e8a2d86b01e8cf831e271f9425c8516e673
GhiathAjam <ghiath.ajam01@eng-st.cu.edu.eg>
2023-05-20 19:10:39 +0200

    preprocessing: contrast+illumination+noise

--- preprocessing.py
+++ preprocessing.py
@@ -1,22 +1,27 @@
 # %% [markdown]
-# # preprocess images for flood detection task
+#  # preprocess images for flood detection task
 
 # %%
 import os
 import tqdm
-from skimage import io
 import numpy as np
-import shutil
+import matplotlib.pyplot as plt
+from skimage import io
 from skimage import data, color
 from skimage.transform import rescale, resize, downscale_local_mean
+# noise
+from skimage.filters import median
+# illumination
+# contrast
+from skimage import exposure
 
 # %% [markdown]
-# # Ideas:
-#     - normalize images shape
-#     - normalize colors
-#     - noise?
-#     - illumination?
-#     - contrast?
+#  # Ideas:
+#      - normalize images shape
+#      - normalize colors
+#      - noise?
+#      - illumination?
+#      - contrast?
 
 # %%
 def resize_img(img, W, H):
@@ -82,10 +87,87 @@ def read_resize_dataset(pth='./dataset/', pth2=None, W=None, H=None, Debug=False
         # save image
         io.imsave(pth2+'non-flooded/'+fil, img)
 
-# %%
-
 
 # %%
+def read_dataset(pth='./dataset/'):
+    # read images
+    flooded = []
+    non_flooded = []
+    for fil in os.listdir(pth + 'flooded/'):
+        img = io.imread(pth+'flooded/'+fil)
+        flooded.append(img)
+    for fil in os.listdir(pth + 'non-flooded/'):
+        img = io.imread(pth+'non-flooded/'+fil)
+        non_flooded.append(img)
+    #
+    X = np.array(flooded + non_flooded)
+    y = np.array([1]*len(flooded) + [0]*len(non_flooded))
+    return X, y
 
+# %%
+## more preprocessing
+# noise
+def remove_noise(img):
+    img2 = img.copy()
+    # use median filter from skimage
+    img2 = median(img2)
+    return img2
+
+# %%
+# illumintaion
+def fix_illumination(img):
+    img2 = img.copy()
+    # use skimage
+    img2 = exposure.equalize_adapthist(img2, clip_limit=0.03)
+    return img2
+
+# %%
+# contrast
+def fix_contrast(img):
+    img2 = img.copy()
+    # use skimage
+    img2 = exposure.equalize_hist(img2)
+    return img2
+
+# %%
+def test_preprocessing():
+    tst = io.imread('./dataset/flooded/0.jpg')
+
+    fig = plt.figure(figsize=(17, 7))
+
+    ax = fig.add_subplot(2, 4, 1)
+    ax.imshow(tst)
+    ax.set_title('Original')
+
+    ax = fig.add_subplot(2, 4, 2)
+    img2 = fix_contrast(tst)
+    ax.imshow(img2)
+    ax.set_title('Contrast')
+
+    ax = fig.add_subplot(2, 4, 3)
+    img2 = remove_noise(tst)
+    ax.imshow(img2)
+    ax.set_title('Noise')
+
+    ax = fig.add_subplot(2, 4, 4)
+    img2 = fix_illumination(tst)
+    ax.imshow(img2)
+    ax.set_title('Illumination')
+
+    ax = fig.add_subplot(2, 4, 5)
+    img2 = fix_contrast(tst)
+    img2 = fix_illumination(img2)
+    ax.imshow(img2)
+    ax.set_title('Contrast + Illumination')
+
+    plt.show()
+
+# %%
+# read_resize_dataset(W=64)
+
+# X, y = read_dataset(pth='./dataset64/')
+# print(X.shape, y.shape)
+
+# test_preprocessing()
 
 
